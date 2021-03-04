e382b39d094b0ec7b9e73003785a1bbd6809414c
wezley <wezley.singleton@gmail.com>
2021-03-04 12:32:01 -0500

    added real logic to controller (hooks up to service now)

--- src/main/java/com/revature/quizzard/web/controllers/UserController.java
+++ src/main/java/com/revature/quizzard/web/controllers/UserController.java
@@ -5,53 +5,40 @@ import com.revature.quizzard.dtos.QuizzardHttpStatus;
 import com.revature.quizzard.exceptions.ResourceNotFoundException;
 import com.revature.quizzard.models.Role;
 import com.revature.quizzard.models.User;
+import com.revature.quizzard.services.UserService;
 import com.revature.quizzard.util.ErrorResponseFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<User> getAllUsers(HttpServletRequest req) {
-        System.out.println(req); // Spring gives you a facade object for lower-level objects from the Servlet API
-        System.out.println(req.getAttribute("example-attribute"));
-        List<User> exampleUsers = new ArrayList<>();
-        exampleUsers.add(new User("wsingleton", "password", "wsingleton@revature.com", "Wezley", "Singleton", Role.ADMIN));
-        exampleUsers.add(new User("skelsey", "p4ssw0rd", "skelsey@revature.com", "Steven", "Kelsey", Role.PREMIUM_USER));
-        exampleUsers.add(new User("njurczak", "drowssap", "njurczak@revature.com", "Nickolas", "Jurczak", Role.DEV));
-        return exampleUsers;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-//    @GetMapping(path = "/id/{id}/something/{stf}")
-//    public User getUserById(@PathVariable int id, @PathVariable("stf") String stuff) {
-//        System.out.println("id provided: " + id);
-//        System.out.println("stuff provided: " + stuff);
-//        return new User("wsingleton", "password", "wsingleton@revature.com", "Wezley", "Singleton", Role.ADMIN);
-//    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Set<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 
-    @GetMapping(path = "/id", produces = MediaType.APPLICATION_JSON_VALUE)
-    public User getUserById(@RequestParam int id, @RequestParam("stf") String stuff) {
-        if (id <= 0) {
-            throw new ResourceNotFoundException();
-        }
-        System.out.println("id provided: " + id);
-        System.out.println("stuff provided: " + stuff);
-        return new User("wsingleton", "password", "wsingleton@revature.com", "Wezley", "Singleton", Role.ADMIN);
+    @GetMapping(path = "/id/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public User createNewUser(@RequestBody User newUser, @RequestHeader String someHeader) {
-        System.out.println(newUser);
-        System.out.println(someHeader);
-        return newUser;
+    public void createNewUser(@RequestBody User newUser) {
+        userService.register(newUser);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -60,4 +47,28 @@ public class UserController {
         return ErrorResponseFactory.getInstance().generateErrorResponse(QuizzardHttpStatus.NOT_FOUND);
     }
 
+    // Included for posterity
+//    @GetMapping(path = "/id/{id}/something/{stf}")
+//    public User getUserById(@PathVariable int id, @PathVariable("stf") String stuff) {
+//        System.out.println("id provided: " + id);
+//        System.out.println("stuff provided: " + stuff);
+//        return new User("wsingleton", "password", "wsingleton@revature.com", "Wezley", "Singleton", Role.ADMIN);
+//    }
+
+//    @GetMapping(path = "/id", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User getUserById(@RequestParam int id, @RequestParam("stf") String stuff) {
+//        if (id <= 0) {
+//            throw new ResourceNotFoundException();
+//        }
+//        System.out.println("id provided: " + id);
+//        System.out.println("stuff provided: " + stuff);
+//        return new User("wsingleton", "password", "wsingleton@revature.com", "Wezley", "Singleton", Role.ADMIN);
+//    }
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+//    public User createNewUser(@RequestBody User newUser, @RequestHeader String someHeader) {
+//        System.out.println(newUser);
+//        System.out.println(someHeader);
+//        return newUser;
+//    }
 }
