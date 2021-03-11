c1cb060f191deb73cefb0c34c4a1a2c05c02d34f
wezley <wezley.singleton@gmail.com>
2021-03-11 12:14:46 -0500

    needed to exclude filter from LoggingAspect pointcut (cannot proxy it due to inherited final methods)

--- src/main/java/com/revature/quizzard/util/aspects/LoggingAspect.java
+++ src/main/java/com/revature/quizzard/util/aspects/LoggingAspect.java
@@ -15,7 +15,7 @@ public class LoggingAspect {
 
     private final Logger logger = LogManager.getLogger(LoggingAspect.class);
 
-    @Pointcut("within(com.revature.quizzard..*)")
+    @Pointcut("within(com.revature.quizzard..*) && !within(com.revature.quizzard.web.filters..*)")
     public void logAllPointcut() {}
 
     @Before("logAllPointcut()")
