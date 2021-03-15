b797ff838b62d5504395f17d1174b3e1768bf1d4
wezley <wezley.singleton@gmail.com>
2021-03-15 12:47:51 -0400

    update UserRepo to extend JpaRepo instead of CrudRepo

--- src/main/java/com/revature/quizzard/repos/UserRepository.java
+++ src/main/java/com/revature/quizzard/repos/UserRepository.java
@@ -1,15 +1,15 @@
 package com.revature.quizzard.repos;
 
 import com.revature.quizzard.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findUserByUsername(String username);
     Set<User> findUsersByRole(String role);
